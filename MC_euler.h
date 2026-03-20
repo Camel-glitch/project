a0a1cea36a487cc3b9b3d05bf656127c8e867a00
Camel-glitch <acrmalik5@gmail.com~>
2026-03-20 12:11:45 +0100

    ...

--- MC_euler.h
+++ MC_euler.h
@@ -2,6 +2,6 @@
 #define MC_EULER_H
 
 __global__ void MC_euler(float rho, float v_0, float S_0, float r, float sigma, float k, float theta, float dt, float K, int N,
-              curandState* state, float* sum, int n);
+              curandState* state, float* sum, float* sum2, int n);
 
 #endif // MC_EULER_H
