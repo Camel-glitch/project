6c668a405f48356384e70d64356422e22f842e74
Camel-glitch <acrmalik5@gmail.com~>
2026-03-29 20:53:32 +0200

    ..

--- gamma.cu
+++ gamma.cu
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <curand_kernel.h>
 #include <math.h>
 #include <curand_kernel.h>
 
 
 __device__ float generate_gamma(float a, curandState *state) {
     if (a < 1.0) {
-        float u = curand_uniform_float(state);
+        float u = curand_uniform(state);
         return generate_gamma(a + 1.0, state) * pow(u, 1.0 / a);
     }
 
@@ -17,12 +16,12 @@ __device__ float generate_gamma(float a, curandState *state) {
 
     while (true) {
         do {
-            x = curand_normal_float(state);//N(0,1)
+            x = curand_normal(state);//N(0,1)
             v = 1.0f + c * x;
         } while (v <= 0);
 
         v = v * v * v;
-        u = curand_uniform_float(state); // U(0,1)
+        u = curand_uniform(state); // U(0,1)
 
         // Squeeze Test (imply the second test and faster)
         if (u < 1.0f - 0.0331f * (x * x) * (x * x)) {
