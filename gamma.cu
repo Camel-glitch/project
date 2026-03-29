1b650b0c9de9ab73ce824eda1e75e40ba5a9175d
Camel-glitch <acrmalik5@gmail.com~>
2026-03-29 20:51:21 +0200

    ..

--- gamma.cu
+++ gamma.cu
@@ -4,33 +4,33 @@
 #include <curand_kernel.h>
 
 
-__device__ double generate_gamma(double a, curandState *state) {
+__device__ float generate_gamma(float a, curandState *state) {
     if (a < 1.0) {
-        double u = curand_uniform_double(state);
+        float u = curand_uniform_float(state);
         return generate_gamma(a + 1.0, state) * pow(u, 1.0 / a);
     }
 
     // Algorithme de Marsaglia-Tsang (a >= 1)
-    double d = a - 1.0 / 3.0;
-    double c = 1.0 / sqrt(9.0 * d);
-    double x, v, u;
+    float d = a - 1.0f / 3.0f;
+    float c = 1.0f / sqrt(9.0f * d);
+    float x, v, u;
 
     while (true) {
         do {
-            x = curand_normal_double(state);//N(0,1)
-            v = 1.0 + c * x;
+            x = curand_normal_float(state);//N(0,1)
+            v = 1.0f + c * x;
         } while (v <= 0);
 
         v = v * v * v;
-        u = curand_uniform_double(state); // U(0,1)
+        u = curand_uniform_float(state); // U(0,1)
 
         // Squeeze Test (imply the second test and faster)
-        if (u < 1.0 - 0.0331 * (x * x) * (x * x)) {
+        if (u < 1.0f - 0.0331f * (x * x) * (x * x)) {
             return d * v;
         }
 
       
-        if (log(u) < 0.5 * x * x + d * (1.0 - v + log(v))) {
+        if (log(u) < 0.5f * x * x + d * (1.0f - v + log(v))) {
             return d * v;
         }
     }
--- gamma.h
+++ gamma.h
@@ -2,7 +2,7 @@
 #define GAMMA_H
 
 // Generating a G(a,1) random variable using the Marsaglia and Tsang method
-__device__ double generate_gamma(double a, curandState *state); 
+__device__ float generate_gamma(float a, curandState *state); 
 
 
 #endif // GAMMA_H
