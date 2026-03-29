23d0fe11aaf849d72a95f254f7e466987f63df9f
Camel-glitch <acrmalik5@gmail.com~>
2026-03-29 20:31:58 +0200

    ..

--- gamma.h
+++ gamma.h
@@ -2,7 +2,7 @@
 #define GAMMA_H
 
 // Generating a G(a,1) random variable using the Marsaglia and Tsang method
-double generate_gamma(double a, curandState *state); 
+__device__ double generate_gamma(double a, curandState *state); 
 
 
 #endif // GAMMA_H
